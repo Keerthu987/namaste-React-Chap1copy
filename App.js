881f61a5fade14d5d865531e5aee1fc85731b728
Keerthana Mouli <u405337@wns.com>
2024-01-12 09:48:42 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,18 @@
+//const heading = React.createElement("h1", { id: "heading" }, "Hello !!");
+
+
+/**const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I'm a H1"), React.createElement("h2", {}, "I'm a H2")]
+    ),
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I'm a H1"), React.createElement("h2", {}, "I'm a H")]
+    )
+    ]
+
+);
+/**console.log(parent);
+
+console.log(jsxHead);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHead); */
